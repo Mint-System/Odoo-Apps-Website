855df1e1041bf7b57f251cbd78f971e2ea17922d
janikvonrotz <contact@janikvonrotz.ch>
2022-04-11 14:41:01 +0200

    update readmes

--- website_date_of_birth/__manifest__.py
+++ website_date_of_birth/__manifest__.py
@@ -1,28 +1,20 @@
 {
-    'name': "Website Date of Birth",
-
-    'summary': """
+    "name": "Website Date of Birth",
+    "summary": """
         Adds date of birth field to partner and website checkout form.
     """,
-
-    'author': "Mint System GmbH, Odoo Community Association (OCA)",
-    'website': "https://www.mint-system.ch",
-    'category': 'Website',
-    'version': '14.0.1.0.1',
-    'license': 'AGPL-3',
-
-    'depends': [
-        'base',
-        'website'
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Website",
+    "version": "14.0.1.0.1",
+    "license": "AGPL-3",
+    "depends": ["base", "website"],
+    "data": [
+        "views/base_view_partner_form.xml",
+        "views/website_sale_address.xml",
+        "data/data.xml",
     ],
-
-    'data': [
-        'views/base_view_partner_form.xml',
-        'views/website_sale_address.xml',
-        'data/data.xml',
-    ],
-
-    'installable': True,
-    'application': False,
+    "installable": True,
+    "application": False,
     "images": ["images/screen.png"],
 }
--- website_product_description/__manifest__.py
+++ website_product_description/__manifest__.py
@@ -1,25 +1,20 @@
 {
-    'name': "Website Product Description",
-
-    'summary': """
+    "name": "Website Product Description",
+    "summary": """
         Add separate description field to website product view.
     """,
-
-    'author': "Mint System GmbH, Odoo Community Association (OCA)",
-    'website': "https://www.mint-system.ch",
-    'category': 'Accounting',
-    'version': '14.0.1.0.1',
-    'license': 'AGPL-3',
-
-    'depends': ['website_sale'],
-
-    'data': [
-        'views/product_product_normal_form_view.xml',
-        'views/product_product_template_only_form_view.xml',
-        'views/website_sale_product.xml',
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Accounting",
+    "version": "14.0.1.0.1",
+    "license": "AGPL-3",
+    "depends": ["website_sale"],
+    "data": [
+        "views/product_product_normal_form_view.xml",
+        "views/product_product_template_only_form_view.xml",
+        "views/website_sale_product.xml",
     ],
-
-    'installable': True,
-    'application': False,
+    "installable": True,
+    "application": False,
     "images": ["images/screen.png"],
 }
