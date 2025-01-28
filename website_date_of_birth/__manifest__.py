bc21425796405ee4790d9b5347b6e604c45dd328
Janik von Rotz <login@janikvonrotz.ch>
2025-01-28 18:19:18 +0100

    chore: update author

--- website_date_of_birth/__manifest__.py
+++ website_date_of_birth/__manifest__.py
@@ -3,7 +3,7 @@
     "summary": """
         Adds date of birth field to partner and website checkout form.
     """,
-    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "author": "Mint System GmbH",
     "website": "https://www.mint-system.ch",
     "category": "Website",
     "version": "15.0.1.1.0",
--- website_product_description/__manifest__.py
+++ website_product_description/__manifest__.py
@@ -3,7 +3,7 @@
     "summary": """
         Add separate description field to website product view.
     """,
-    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "author": "Mint System GmbH",
     "website": "https://www.mint-system.ch",
     "category": "Accounting",
     "version": "15.0.1.1.0",
