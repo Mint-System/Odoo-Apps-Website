7aa169f0e3e9bb65e793d576ad12d877642f36b0
Ulrich Kilian <uk@science-and-more.de>
2026-01-21 22:07:06 +0100

    feat(website_sale_permit_extra_info): store chosen pricelist to session

---
+++ website_sale_permit_extra_info/controllers/website_pricelist.py
@@ -0,0 +1,28 @@
+from odoo import http
+from odoo.http import request
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+
+
+class WebsiteSalePricelistPersist(WebsiteSale):
+
+    @http.route(
+        ['/shop/change_pricelist/<model("product.pricelist"):pricelist>'],
+        type='http',
+        auth="public",
+        website=True,
+        sitemap=False
+    )
+    def pricelist_change(self, pricelist, **post):
+        response = super().pricelist_change(pricelist, **post)
+
+        # Store chosen pricelist in session
+        request.session['chosen_pricelist_id'] = pricelist.id
+
+        # Persist for logged-in users
+        user = request.env.user
+        if not user._is_public():
+            user.partner_id.sudo().write({
+                'property_product_pricelist': pricelist.id
+            })
+
+        return response
