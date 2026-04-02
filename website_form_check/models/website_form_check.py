15a5eda75d441547de99dc654684ff632f4097bf
laurens kooijman <laurenskooijman@gmail.com>
2026-04-02 11:35:58 +0200

    feat(website_form_check):help text und standard wert an patterns Feld hinzugefügt

--- website_form_check/models/website_form_check.py
+++ website_form_check/models/website_form_check.py
@@ -12,7 +12,7 @@ class WebsiteFormCheck(models.Model):
     _description = "Website Form Check"
 
     name = fields.Char()
-    patterns = fields.Char()
+    patterns = fields.Char(default="max.*muster.*max\\.muster@beispiel\\.com.*20.02.1980", help="Gebe hier das Muster an, das kontrolliert werden soll. Trage 'Vorname', 'Nachname', 'E-Mailadresse' und 'Geburtsdatum' in genau dieser Reihenfolge ein, mit '.*' als Trennzeichen. NB Eine '.' im Muster muss als '\\.' geschrieben werden, z. B. in der E-Mailadresse ('kunde@beispiel\\.com'), aber nicht beim Trennzeichen ('Max.*Muster').")
     redirect_id = fields.Many2one(comodel_name="website.rewrite")
     website_id = fields.Many2one(
         comodel_name="website",
